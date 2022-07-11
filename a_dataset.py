3a54edc7521cc6aa3cc0bcb702a58519cce7d0d1
hatimbr <bourfoune.hatim@gmail.com>
2022-07-11 17:34:26 +0200

    add new datasets implementation

--- a_dataset.py
+++ a_dataset.py
@@ -1,6 +1,6 @@
 from torch.utils.data import DataLoader
-from monai.data import CacheDataset
-from monai.transforms import Compose, EnsureChannelFirstd, LoadImaged, SpatialCropd
+from monai.data import Dataset
+from monai.transforms import Compose, EnsureChannelFirstd, LoadImaged, SpatialCropd, ConcatItemsd
 import os
 
 
@@ -9,9 +9,13 @@ def get_datalist(source):
     with open(os.path.join(source, "train.txt"), "r") as f:
         lines = f.readlines()
         for line in lines:
+            file_dic = {}
             files = line.split()
-            datalist.append({"image": os.path.join(source, files[0]),
-                             "label": os.path.join(source, files[1])})
+            for i, file in enumerate(files[:-1]):
+                file_dic[f"channel_{i}"] = os.path.join(source, file)
+
+            file_dic["label"] = os.path.join(source, files[-1])
+            datalist.append(file_dic)
 
     print("got datalist, extract: \n", datalist[0])
     return datalist
@@ -20,21 +24,22 @@ def get_datalist(source):
 def get_dataset(source, batch_size=2, limit_sample=None): # Only work for the Task01_BrainTumour dataset for now
     print("loading dataset...")
     datalist = get_datalist(source)
+    keys = list(datalist[0].keys())
     if limit_sample: datalist = datalist[:limit_sample]
 
     transform = Compose([
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys=["image", "label"]),
-        SpatialCropd(keys=["image", "label"],
-                     roi_center=[120, 120, 77],
-                     roi_size=[128, 128, 128]) # crop it to make easily usable
+        LoadImaged(keys=keys),
+        EnsureChannelFirstd(keys=keys),
+        ConcatItemsd(keys[:-1], "input"),
+        SpatialCropd(keys=['input', 'label'], # crop it to make easily usable for etape 1
+                     roi_size=[128, 128, 128],
+                     roi_center=[0, 0, 0]
+                     )
     ])
 
-    train_ds = CacheDataset( # CacheDataset might not be a good choice in the future
+    train_ds = Dataset( # A more optimized dataset can be used
         data=datalist,
-        transform=transform,
-        num_workers=8,
-        cache_rate=1.0,
+        transform=transform
     )
 
     data_loader = DataLoader(
